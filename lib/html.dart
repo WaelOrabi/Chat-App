458b9bf163710b6fa1f0af95886e8fa897a00b49
Wael Orabi <waelorabi267@gmail.com>
2022-04-22 14:12:29 +0300

    add file html

---
+++ lib/html.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Html extends StatelessWidget {
+  const Html({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('fewed'),
+    );
+  }
+}
